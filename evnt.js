8d3a03192dd0f86ee40b2583edc3c603eca080a0
kailash29 <kailash.vishnoi@incture.com>
2017-04-04 12:00:59 +0530

    for pull request

--- evnt.js
+++ evnt.js
@@ -5,7 +5,12 @@ var eventEmitter = new events.EventEmitter();
 eventEmitter.on('doorOpen', function (msg) {
 	console.log(msg)
 });
+eventEmitter.on('doorOpen1', function (msg) {
+	console.log(msg)
+});
+
 
 eventEmitter.emit('doorOpen', 'ring ring');
+eventEmitter.emit('doorOpen1', 'ring ring');
 
 
